7a76031bd8c8bc393240739b70065eb2a79161d6
Roman Smushkevich <karmara.roman@gmail.com>
2023-03-31 18:40:39 +0200

    * Apis excluded from git.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("io.swagger.core.v3.swagger-gradle-plugin") version "2.2.8" apply false
 }
 
-configure(subprojects.filter { !listOf("web-ui").contains(it.name) }) {
+configure(subprojects) {
     apply {
         plugin("org.springframework.boot")
         plugin("io.spring.dependency-management")
